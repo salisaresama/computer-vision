a1cbd7fa951ea18ddb76d4842f3995e2b324134b
Vladislav Belov <vlbelov@deloittece.com>
2020-03-28 01:12:49 +0100

    --preps for more evaluations;

--- flann.py
+++ flann.py
@@ -4,23 +4,24 @@ from pylib.ann.cluster import CustomKMeans
 
 
 if __name__ == '__main__':
-	# Reading the SIFT data set
+    # Reading the SIFT data set
     X = np.fromfile('./data/SIFT/SIFT.dat', dtype=np.uint8).astype(np.float32)
     X = X.reshape((-1, 128))
     print(f'Descriptors format: {X.shape}')
-	
-	# Evaluate
-    METHOD = 'exact'
+
+    # Evaluate
+    METHOD = 'kmeans'
     print(f'Method: {METHOD}')
     km = CustomKMeans(
-    	n_centers=32000, 
-    	method=METHOD,
-    	nn_autotune=0.99,
-    	nn_checks=100,
-    	nn_kd_trees=64,
-    	nn_km_branching=64,
-    	nn_km_iter=20,
-    	verbose=True
+        n_centers=32000,
+        method=METHOD,
+        nn_autotune=-1,
+        nn_checks=25,
+        nn_kd_trees=64,
+        nn_km_branching=8,
+        nn_km_iter=10,
+        apply_fix=False,
+        verbose=True
     )
     km.fit(X)
     print(km.time_report())
