f8ad22f41c96190c20018fb89c4d54f26b494ee2
Vladislav Belov <belovvla@gpu1.felk.cvut.cz>
2020-06-05 22:43:16 +0200

    32K run

--- code/task4_run.py
+++ code/task4_run.py
@@ -2,9 +2,11 @@ import pandas as pd
 import numpy as np
 import os
 from cluster import CustomKMeans
+import faiss
 
 
 if __name__ == '__main__':
+    print(f'n_gpus: {faiss.get_num_gpus()}')
     # Reading the SIFT data set
     X = np.fromfile('../data/SIFT.dat', dtype=np.uint8).astype(np.float32)
     X = X.reshape((-1, 128))
@@ -14,7 +16,7 @@ if __name__ == '__main__':
     METHOD = 'exact-gpu'
     print(f'Method: {METHOD}')
     km = CustomKMeans(
-        n_centers=100,
+        n_centers=32000,
         max_iter=30,
         gpu_idx=1,
         method=METHOD,
